ac62bd5c775973c3137da3fdf89b4eb1ca0b7f29
Arjun.Avagadda <33811659+arjunavagadda@users.noreply.github.com>
2020-05-19 18:43:49 +0530

    ..

--- vars/shared.groovy
+++ vars/shared.groovy
@@ -1,5 +1,5 @@
 def call(String buildStatus = "STARTED"){
-    build Status = build Status ?: 'SUCCESSFUL'
+    buildStatus = buildStatus ?: 'SUCCESSFUL'
 
     sparkSend credentialsId: '15e9eb64-1714-4a78-a9cb-674cfe3c6202', 
     message: "${JOB_NAME}${JOB_URL} ${currentBuild.currentResult} at ${env.STAGE_NAME}", messageType: 'text', spaceList: [[spaceId: '7f719b30-860d-11ea-9583-c154a39de77b']]
