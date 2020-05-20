55dc88410755a941a8fd675c2b2e19c179ce3c25
Arjun.Avagadda <33811659+arjunavagadda@users.noreply.github.com>
2020-05-20 13:24:48 +0530

    modfy

--- vars/shared.groovy
+++ vars/shared.groovy
@@ -1,6 +1,6 @@
-def call(String buildStatus = "STARTED"){
+def call(String "STARTED"){
      
-    switch(currentBuild.result) {
+    switch('${currentBuild.currentResult}') {
     case 'FAILURE':
          sparkSend credentialsId: '15e9eb64-1714-4a78-a9cb-674cfe3c6202', 
        message: "${JOB_NAME}${JOB_URL} <h1>${currentBuild.currentResult}</h1> at ${env.STAGE_NAME}", messageType: 'html', spaceList: [[spaceId: '7f719b30-860d-11ea-9583-c154a39de77b']]
