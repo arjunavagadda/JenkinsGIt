d44f16451d7e1d0589f2e16f0e173bfe85f759f8
Arjun.Avagadda <33811659+arjunavagadda@users.noreply.github.com>
2020-05-20 13:07:12 +0530

    getresult function

--- vars/shared.groovy
+++ vars/shared.groovy
@@ -1,10 +1,22 @@
 def call(String buildStatus = "STARTED"){
-    def FAILED_STAGE
-
-    FAILED_STAGE=env.STAGE_NAME
-
-    sparkSend credentialsId: '15e9eb64-1714-4a78-a9cb-674cfe3c6202', 
-    message: "${JOB_NAME}${JOB_URL} ${currentBuild.currentResult} at ${env.STAGE_NAME}", messageType: 'text', spaceList: [[spaceId: '7f719b30-860d-11ea-9583-c154a39de77b']]
+     
+    getResult()
          
 
+}
+
+def getResult() {
+  switch(currentBuild.result) {
+    case 'FAILURE':
+         echo 'fail'
+    
+    case 'SUCCESS':
+       echo 'success'
+       sparkSend credentialsId: '15e9eb64-1714-4a78-a9cb-674cfe3c6202', 
+       message: "${JOB_NAME}${JOB_URL} ${currentBuild.currentResult} at ${env.STAGE_NAME}", messageType: 'text', spaceList: [[spaceId: '7f719b30-860d-11ea-9583-c154a39de77b']]
+    
+    
+    default:
+      return 'unknown'
+  }
 }
