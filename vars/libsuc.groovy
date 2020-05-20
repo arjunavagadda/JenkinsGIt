08def2cf5931322b496bbf1d61f75cab1919e768
Arjun.Avagadda <33811659+arjunavagadda@users.noreply.github.com>
2020-05-20 19:05:38 +0530

    ...

--- vars/libsuc.groovy
+++ vars/libsuc.groovy
@@ -4,5 +4,9 @@ def call(){
        message: "${JOB_NAME}${JOB_URL} ${currentBuild.currentResult}", messageType: 'text', spaceList: [[spaceId: '7f719b30-860d-11ea-9583-c154a39de77b']]
 }
 
+def call(String filter_string){
 
+       sparkSend credentialsId: '15e9eb64-1714-4a78-a9cb-674cfe3c6202', 
+       message: "${JOB_NAME}${JOB_URL} <h1>${currentBuild.currentResult}</h1>", messageType: 'html', spaceList: [[spaceId: '7f719b30-860d-11ea-9583-c154a39de77b']]
+}
 
