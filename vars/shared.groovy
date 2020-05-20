c0cc79faca885cee64e1ce7cc64b20d20f51746f
Arjun.Avagadda <33811659+arjunavagadda@users.noreply.github.com>
2020-05-20 16:30:08 +0530

    Result

--- vars/shared.groovy
+++ vars/shared.groovy
@@ -2,17 +2,11 @@ def call(String Result="Started"){
 
          sparkSend credentialsId: '15e9eb64-1714-4a78-a9cb-674cfe3c6202', 
        message: "${JOB_NAME}${JOB_URL} <h1>${currentBuild.currentResult}</h1> at ${env.STAGE_NAME}", messageType: 'html', spaceList: [[spaceId: '7f719b30-860d-11ea-9583-c154a39de77b']]
-   
-   
-  
-       
 }
 
-def fail(String Stat="Failed"){
+def fail(String Result="Failed"){
 
     sparkSend credentialsId: '15e9eb64-1714-4a78-a9cb-674cfe3c6202', 
        message: "${JOB_NAME}${JOB_URL} ${currentBuild.currentResult} at ${env.STAGE_NAME}", messageType: 'text', spaceList: [[spaceId: '7f719b30-860d-11ea-9583-c154a39de77b']]
-    
-
 }
 
