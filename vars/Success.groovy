7982624fb5bb39f24650a9867a24fe7d90f027d7
Arjun.Avagadda <33811659+arjunavagadda@users.noreply.github.com>
2020-05-20 16:43:57 +0530

    final change

--- vars/Success.groovy
+++ vars/Success.groovy
@@ -1,4 +1,4 @@
-def call(String Result="Success"){
+def call(String Result="STARTED"){
 
        sparkSend credentialsId: '15e9eb64-1714-4a78-a9cb-674cfe3c6202', 
        message: "${JOB_NAME}${JOB_URL} ${currentBuild.currentResult}", messageType: 'text', spaceList: [[spaceId: '7f719b30-860d-11ea-9583-c154a39de77b']]
