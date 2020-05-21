83a3b1b255bffa538626db49eedf533cf25c1764
Arjun.Avagadda <arjunavagadda207@gmail.com>
2020-05-21 09:11:52 +0530

    Original
    Unchanged pull this

--- vars/libsuc.groovy
+++ vars/libsuc.groovy
@@ -6,10 +6,9 @@ def call(){
 
 def call(String filter_string){
        
-        def color = 'RED'
-        def colorCode = '#FF0000'
+        
 
        sparkSend credentialsId: '15e9eb64-1714-4a78-a9cb-674cfe3c6202', 
-       message: "${JOB_NAME}${JOB_URL} ${currentBuild.currentResult}",color: colorCode, messageType: 'html', spaceList: [[spaceId: '7f719b30-860d-11ea-9583-c154a39de77b']]
+       message: "${JOB_NAME}${JOB_URL} <h3>${currentBuild.currentResult}</h3>",messageType: 'html', spaceList: [[spaceId: '7f719b30-860d-11ea-9583-c154a39de77b']]
 }
 
