d90894ee424e57ea0695b5e0b722a39e69cef7de
Arjun.Avagadda <33811659+arjunavagadda@users.noreply.github.com>
2020-05-25 19:01:53 +0530

    csv file

--- vars/dynamicstage.groovy
+++ vars/dynamicstage.groovy
@@ -1,8 +1,16 @@
+@Grab('com.xlson.groovycsv:groovycsv:1.0')
+import static com.xlson.groovycsv.CsvParser.parseCsv
 
 def call(){
 
      def request = libraryResource 'demo.csv'
      println "${request}"
+
+     def data = parseCsv(request)
+
+     for(line in data) {
+    println "${line}"
+    }
 
 
 }
