59715434ae0a4c19eaae3d6c1a2663334e42152c
Arjun.Avagadda <arjunavagadda207@gmail.com>
2020-07-20 18:49:04 +0530

    Create json.groovy

---
+++ vars/json.groovy
@@ -0,0 +1,19 @@
+@Grab('com.xlson.groovycsv:groovycsv:1.0')
+import static com.xlson.groovycsv.CsvParser.parseCsv
+
+def call(){
+
+     def request = libraryResource 'demo.csv'
+     println "${request}"
+
+     def data = parseCsv(request, readFirstLine:true,
+                    columnNames:['FirstName', 'Country'])
+
+     for(line in data) {
+
+    println "$line.FirstName ${line[1]}"
+
+    }
+
+
+}
