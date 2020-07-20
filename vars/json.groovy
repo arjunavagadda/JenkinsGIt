51de5ed24163643cd6692ae3ae1bbcee4ca1f058
Arjun.Avagadda <arjunavagadda207@gmail.com>
2020-07-20 22:17:32 +0530

    Update json.groovy

--- vars/json.groovy
+++ vars/json.groovy
@@ -9,11 +9,11 @@ def call(job){
 
      def InputJSON = new JsonSlurper().parseText(request)
 
-     println job
+     // println job
 
-     result = InputJSON.job.Name
+     // result = InputJSON.job.Name
      
-     println result
+     // println result
 
      
 
